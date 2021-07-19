7585cfb5c28e54c17fc281e0a168ee0bbc7eefee
Sullied Security <sulliedeclat@gmail.com>
2021-07-18 21:57:00 -0400

    Added utility class of location functions
    Added functions for Mercator projection of single values and arrays.

---
+++ Sources/Visualization/Location.swift
@@ -0,0 +1,26 @@
+import Foundation
+
+// TODO: The "accelerate" library with vector operations
+
+class Location {
+    // Project a single latitude and longitude point using the Mercator projection
+    // https://mathworld.wolfram.com/MercatorProjection.html
+    static func project(latitude: Double, longitude: Double) -> (Double, Double) {
+        let xs = toRadians(longitude)
+        let ys = log(tan(Double.pi / 4 + toRadians(latitude) / 2))
+        return (xs, ys)
+    }
+
+    // Project an array of latitude and longitude points using the Mercator projection
+    // https://mathworld.wolfram.com/MercatorProjection.html
+    static func project(latitude: [Double], longitude: [Double]) -> ([Double], [Double]) {
+        let ys = longitude.map { toRadians($0) }
+        let xs = latitude.map { log(tan(Double.pi / 4 + toRadians($0) / 2)) }
+        return (xs, ys)
+    }
+
+    // Convert a value from degrees to radians
+    static func toRadians(_ degrees: Double) -> Double {
+        return Double.pi / 180 * degrees
+    }
+}
