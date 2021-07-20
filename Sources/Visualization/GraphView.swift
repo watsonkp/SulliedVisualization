570d6d93dda39297547e678ad68d6fb70980f574
Sullied Security <sulliedeclat@gmail.com>
2021-07-19 20:45:20 -0400

    Added color choice for data in graph visualization
    Added optional parameters for the color of data to both single datasets
    and multiple dataset initializers.
    Changed preview to make use of multiple colors.

--- Sources/Visualization/GraphView.swift
+++ Sources/Visualization/GraphView.swift
@@ -9,6 +9,8 @@ public struct GraphView: View {
     let maxX: Double
     let minY: Int
     let maxY: Int
+    let color: Color?
+    var colors: [Color]?
 
     let xAxis: TimeAxis
     let yAxis: ValueAxis
@@ -24,10 +26,10 @@ public struct GraphView: View {
                     ZStack {
                         GridLineView(width: proxy.size.width, height: proxy.size.height - 50, count: 6)
                         if let x = x, let y = y {
-                            DataView(width: proxy.size.width - 50, height: proxy.size.height - 50, minX: 0, maxX: 40 * 60, minY: 80, maxY: 200, xs: x, ys: y)
+                            DataView(width: proxy.size.width - 50, height: proxy.size.height - 50, minX: 0, maxX: 40 * 60, minY: 80, maxY: 200, xs: x, ys: y, color: color!)
                         } else if let xs = xs, let ys = ys {
                             ForEach(xs.indices, id: \.self) { index in
-                                DataView(width: proxy.size.width - 50, height: proxy.size.height - 50, minX: 0, maxX: 40 * 60, minY: 80, maxY: 200, xs: xs[index], ys: ys[index])
+                                DataView(width: proxy.size.width - 50, height: proxy.size.height - 50, minX: 0, maxX: 40 * 60, minY: 80, maxY: 200, xs: xs[index], ys: ys[index], color: colors![index])
                             }
                         }
                     }
@@ -40,7 +42,7 @@ public struct GraphView: View {
         }
     }
 
-    public init(x: [Double], y: [Int]) {
+    public init(x: [Double], y: [Int], color: Color = Color.blue) {
         self.x = x
         self.y = y
 
@@ -49,14 +51,17 @@ public struct GraphView: View {
         self.minY = y.min()!
         self.maxY = y.max()!
 
+        self.color = color
+
         self.xAxis = TimeAxis(min: self.minX, max: self.maxX, count: 5)
         self.yAxis = ValueAxis(min: self.minY, max: self.maxY, count: 5)
 
         self.xs = nil
         self.ys = nil
+        self.colors = nil
     }
 
-    public init(data: [([Double], [Int])]) {
+    public init(data: [([Double], [Int])], colors: [Color] = [Color.blue]) {
         self.xs = data.map {$0.0}
         self.ys = data.map {$0.1}
 
@@ -65,11 +70,17 @@ public struct GraphView: View {
         self.minY = data.map({$0.1.min()!}).min()!
         self.maxY = data.map({$0.1.max()!}).max()!
 
+        self.colors = colors
+        while xs!.count > self.colors!.count {
+            self.colors!.append(contentsOf: colors)
+        }
+
         self.xAxis = TimeAxis(min: self.minX, max: self.maxX, count: 5)
         self.yAxis = ValueAxis(min: self.minY, max: self.maxY, count: 5)
 
         self.x = nil
         self.y = nil
+        self.color = nil
     }
 }
 
@@ -81,6 +92,6 @@ struct GraphView_Previews: PreviewProvider {
 
         let splitRecords: [Record] = load("2021-07-03-13-56-44.json")
         let splitPaths = parseAll(splitRecords)
-        GraphView(data: splitPaths)
+        GraphView(data: splitPaths, colors: [Color.red, Color.green, Color.blue])
     }
 }
