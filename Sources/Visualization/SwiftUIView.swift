9a4421514759dee6209bba3f33eee3506cf1151b
Sullied Security <sulliedeclat@gmail.com>
2022-03-22 16:11:41 -0400

    Increase iOS platform version
    Changed iOS platform version to 15 for primary and secondary Color
    constants.

---
+++ Sources/Visualization/SwiftUIView.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by Kevin Watson on 2022-03-15.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
