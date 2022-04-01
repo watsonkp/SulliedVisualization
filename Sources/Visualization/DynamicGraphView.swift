c73f998344a8aa62fbcc1f770261cac9f3b8038a
Sullied Security <sulliedeclat@gmail.com>
2022-04-01 12:35:00 -0400

    Added handling of gestures while in progress
    Added updating callback for transient pan and zoom values to provide
    feedback to the user and a more pleasant feel.
    Removed dynamic y range to simplify the transient gesture
    implementations. Need to consider the goals and value of
    reimplementation.

--- Sources/Visualization/DynamicGraphView.swift
+++ Sources/Visualization/DynamicGraphView.swift
@@ -57,16 +57,42 @@ public struct DynamicGraphView: View {
     let colors: [Color]
     let xRange: (CGFloat, CGFloat)
     let yRange: (CGFloat, CGFloat)
-    @State var magnification: Double = 1.0
-    @State var visibleStartIndex: Int
-    @State var visibleEndIndex: Int
-    @State var visibleXRange: (CGFloat, CGFloat)
-    @State var visibleYRange: (CGFloat, CGFloat)
-    @State var xLabels: [String]
-    @State var yLabels: [String]
+    var visibleStartIndex: Int {
+        get {
+            let width = (xRange.1 - xRange.0) / (zoom * partialZoom)
+            let xStart = pan + partialPan - width / 2
+            return dataPoints.firstIndex(where: { $0.x >= xStart }) ?? 0
+        }
+    }
+    var visibleEndIndex: Int {
+        get {
+            let width = (xRange.1 - xRange.0) / (zoom * partialZoom)
+            let xStart = pan + partialPan - width / 2
+            let xEnd = xStart + (xRange.1 - xRange.0) / (zoom * partialZoom)
+            return dataPoints.lastIndex(where: { $0.x <= xEnd }) ?? dataPoints.count
+        }
+    }
+    var visibleXRange: (CGFloat, CGFloat) {
+        get {
+            let width = (xRange.1 - xRange.0) / (zoom * partialZoom)
+            return (pan + partialPan - width / 2, pan + partialPan + width / 2)
+        }
+    }
+    var xLabels: [String] {
+        get {
+            let xIncrement = (self.visibleXRange.1 - self.visibleXRange.0) / 4.0
+            return stride(from: self.visibleXRange.0, to: self.visibleXRange.1, by: xIncrement)
+                .map({ String(format: "%.1f", $0) })
+        }
+    }
+    let yLabels: [String]
     let showZones: Bool
     let zoneMaximum: Double?
     @State var isInteracting = false
+    @GestureState private var partialZoom: CGFloat = 1.0
+    @State var zoom: CGFloat = 1.0
+    @GestureState private var partialPan: CGFloat = 0.0
+    @State private var pan: CGFloat
 
     public var body: some View {
         StaticGraphView(data: dataPoints, xRange: xRange, yRange: yRange, showZones: showZones, zoneMaximum: zoneMaximum)
@@ -84,34 +110,52 @@ public struct DynamicGraphView: View {
                             }
                         }
                         GeometryReader { proxy in
-                            DataViewV2(data: dataPoints[visibleStartIndex..<visibleEndIndex], xRange: visibleXRange, yRange: visibleYRange, showZones: showZones, zoneMaximum: zoneMaximum)
-                            .gesture(MagnificationGesture().onEnded { value in
-                                // TODO: It seems like magnification can be infinity.
-                                // TODO: Handle large magnifications with exponential decay and a hard limit? min(10.0, Double.infinity) works.
-                                self.magnification = max(1.0, value * self.magnification)
-                                updateMagnification()
-                                updateXLabels()
-                                updateYLabels()
-                            })
-                            .gesture(DragGesture().onEnded { value in
-                                // Don't pan without zoom. Panning fits y data and doesn't force the inclusion of 0.
-                                // Panning at 1.0 zoom only adjusts to fit the y values, which feels surprising and unintuitive.
-                                if magnification == 1.0 {
-                                    return
-                                }
-                                let maxLeft = self.visibleXRange.0 - self.xRange.0
-                                let maxRight = self.xRange.1 - self.visibleXRange.1
-                                var drag = (self.visibleXRange.1 - self.visibleXRange.0) * (value.location.x - value.startLocation.x) / proxy.size.width
-                                // A pan > 0 is exposing more data from the left. maxLeft >= 0. A pan < 0 is unaffected by this statement.
-                                drag = min(drag, maxLeft)
-                                // A pan < 0 is exposing more data from the right. maxRight >= 0. A pan > 0 is unaffected by this statement.
-                                drag = max(drag, -maxRight)
-                                // Translate the data in the opposite direction of the drag aka "Natural scrolling".
-                                applyPan(-drag)
-                                updateXLabels()
-                                // Y axis labels may change after panning if the visible minimum and or maximum change.
-                                updateYLabels()
-                            })
+                            DataViewV2(data: dataPoints[visibleStartIndex..<visibleEndIndex], xRange: visibleXRange, yRange: yRange, showZones: showZones, zoneMaximum: zoneMaximum)
+                            // DragGesture needs to precede MagnificationGesture or its updating() callback will not be called
+                                .gesture(DragGesture()
+                                    .updating($partialPan) { value, state, transaction in
+                                        let width = (xRange.1 - xRange.0) / zoom
+                                        state = -1 * width * (value.location.x - value.startLocation.x) / proxy.size.width
+                                    }
+                                    .onEnded { value in
+                                        // Show all data and force the inclusion of 0, 0 when magnification is 1.0
+                                        if zoom == 1.0 {
+                                            pan = (xRange.1 - xRange.0) / 2
+                                            return
+                                        }
+                                        let width = (xRange.1 - xRange.0) / zoom
+                                        // Translate the data in the opposite direction of the drag aka "Natural scrolling".
+                                        var translation = -1 * width * (value.location.x - value.startLocation.x) / proxy.size.width
+                                        // Limit drags to the right by the remaining off screen range to the left
+                                        translation = max(-1 * (pan - width / 2 - xRange.0), translation)
+                                        // Limit drags to the left by the remaining off screen range to the right
+                                        translation = min(xRange.1 - (pan + width / 2), translation)
+                                        pan += translation
+                                    })
+                                .gesture(MagnificationGesture()
+                                         // TODO: It seems like magnification can be infinity.
+                                         // TODO: Handle large magnifications with exponential decay and a hard limit? min(10.0, Double.infinity) works.
+                                    .updating($partialZoom) { value, state, transaction in
+                                        state = value
+                                    }
+                                    .onEnded { value in
+                                        // Set a lower limit of 1.0 for magnification.
+                                        if value * zoom <= 1.0 {
+                                            zoom = 1.0
+                                            pan = (xRange.1 - xRange.0) / 2
+                                            return
+                                        }
+                                        zoom = value * zoom
+                                        let width = (xRange.1 - xRange.0) / zoom
+                                        let leftLimit = xRange.0 + width / 2
+                                        let rightLimit = xRange.1 - width / 2
+                                        // Reducing zoom to an ending value above 1.0 can reveal empty x range above or below the limits. Update pan to compensate.
+                                        if pan < leftLimit || pan > rightLimit {
+                                            var resetPan = max(leftLimit, pan)
+                                            resetPan = min(rightLimit, resetPan)
+                                            pan = resetPan
+                                        }
+                                })
                         }
                     }
                     HStack(spacing: 0) {
@@ -136,16 +180,9 @@ public struct DynamicGraphView: View {
         self.colors = [color]
         // Include x=0 and use a range of [0.0, 1.0] when min and max fail due to missing data
         self.xRange = (min(0.0, CGFloat(x.min() ?? 0.0)), CGFloat(x.max() ?? 1.0))
+        self.pan = (self.xRange.1 - self.xRange.0) / 2
         // Include y=0 and use a range of [0.0, 1.0] when min and max fail due to missing data
         self.yRange = (min(0.0, CGFloat(y.min() ?? 0.0)), CGFloat(y.max() ?? 1.0))
-        self.visibleStartIndex = 0
-        self.visibleEndIndex = x.count
-        self.visibleXRange = self.xRange
-        self.visibleYRange = self.yRange
-        self.xLabels = stride(from: self.xRange.0,
-                              to: self.xRange.1,
-                              by: (self.xRange.1 - self.xRange.0) / 4.0)
-        .map({ String(format: "%.1f", $0) })
         self.yLabels = stride(from: self.yRange.0,
                               to: self.yRange.1,
                               by: (self.yRange.1 - self.yRange.0) / 5.0)
@@ -167,18 +204,11 @@ public struct DynamicGraphView: View {
         // Include x=0 and use a range of [0.0, 1.0] when min and max fail due to missing data
         self.xRange = (min(0.0, self.dataPoints.min(by: { $0.x < $1.x })?.x ?? 0.0),
                        self.dataPoints.max(by: { $0.x < $1.x })?.x ?? 1.0)
+        self.pan = (self.xRange.1 - self.xRange.0) / 2
         // Include y=0 and use a range of [0.0, 1.0] when min and max fail due to missing data
         let yRangeMax = self.dataPoints.max(by: { $0.y < $1.y })?.y ?? 1.0
         self.yRange = (min(0.0, self.dataPoints.min(by: { $0.y < $1.y })?.y ?? 0.0),
                        yRangeMax)
-        self.visibleStartIndex = 0
-        self.visibleEndIndex = self.dataPoints.count
-        self.visibleXRange = self.xRange
-        self.visibleYRange = self.yRange
-        self.xLabels = stride(from: self.xRange.0,
-                              to: self.xRange.1,
-                              by: (self.xRange.1 - self.xRange.0) / 4.0)
-        .map({ String(format: "%.1f", $0) })
         self.yLabels = stride(from: self.yRange.0,
                               to: self.yRange.1,
                               by: (self.yRange.1 - self.yRange.0) / 5.0)
@@ -212,65 +242,6 @@ public struct DynamicGraphView: View {
                          [Color.red, Color.yellow, Color.green, Color.blue, Color.gray])
         .map({ Zone(minimum: $0.0 - zoneMax / 10.0, maximum: $0.0, color: $0.1) })
     }
-
-    func updateXLabels() {
-        let xIncrement = (self.visibleXRange.1 - self.visibleXRange.0) / 4.0
-        self.xLabels = stride(from: self.visibleXRange.0, to: self.visibleXRange.1, by: xIncrement)
-            .map({ String(format: "%.1f", $0) })
-    }
-
-    func updateYLabels() {
-        let yIncrement = (self.visibleYRange.1 - self.visibleYRange.0) / 5.0
-        self.yLabels = stride(from: self.visibleYRange.0, to: self.visibleYRange.1, by: yIncrement)
-            .map({ String(format: "%.1f", $0) }).reversed()
-    }
-
-    func updateMagnification() {
-        // Show all data and force the inclusion of 0, 0 when magnification is 1.0
-        if magnification == 1.0 {
-            self.visibleStartIndex = 0
-            self.visibleEndIndex = dataPoints.count
-            self.visibleXRange = self.xRange
-            self.visibleYRange = self.yRange
-            return
-        }
-
-        // Update data visiblity when magnification != 1.0
-        let magnificationOffset: CGFloat = ((self.xRange.1 - self.xRange.0) / self.magnification) / 2
-        let midPoint: CGFloat = (self.visibleXRange.1 + self.visibleXRange.0) / 2
-        // Don't extend beyond the range of the x values in either direction
-        let xStart = max(self.xRange.0, midPoint - magnificationOffset)
-        let xEnd = min(self.xRange.1, midPoint + magnificationOffset)
-        guard let startIndex = dataPoints.firstIndex(where: { $0.x >= xStart }),
-              let endIndex = dataPoints.lastIndex(where: { $0.x <= xEnd }) else {
-            return
-        }
-        self.visibleStartIndex = startIndex
-        self.visibleEndIndex = endIndex
-        self.visibleXRange = (xStart, xEnd)
-        self.updateVisibleYRange()
-    }
-
-    func applyPan(_ pan: CGFloat) {
-        self.visibleXRange = (self.visibleXRange.0 + pan, self.visibleXRange.1 + pan)
-        guard let startIndex = dataPoints.firstIndex(where: { $0.x >= self.visibleXRange.0 }),
-              let endIndex = dataPoints.lastIndex(where: { $0.x <= self.visibleXRange.1 }) else {
-            return
-        }
-        self.visibleStartIndex = startIndex
-        self.visibleEndIndex = endIndex
-        self.updateVisibleYRange()
-    }
-
-    func updateVisibleYRange() {
-        self.visibleYRange = (dataPoints[visibleStartIndex...visibleEndIndex].min(by: { $0.y < $1.y })?.y ?? self.yRange.0,
-                              dataPoints[visibleStartIndex...visibleEndIndex].max(by: { $0.y < $1.y })?.y ?? self.yRange.1)
-        let minimumYRange = (self.yRange.1 - self.yRange.0) / 100
-        if (self.visibleYRange.1 - self.visibleYRange.0) < minimumYRange {
-            let yRangeMidpoint = (self.visibleYRange.0 + self.visibleYRange.1) / 2
-            self.visibleYRange = (yRangeMidpoint - minimumYRange / 2, yRangeMidpoint + minimumYRange / 2)
-        }
-    }
 }
 
 struct DynamicGraphView_Previews: PreviewProvider {
@@ -293,7 +264,6 @@ struct DynamicGraphView_Previews: PreviewProvider {
         DynamicGraphView(x: xInt1 + xInt2 + xInt3, y: yInt1 + yInt2 + yInt3, color: Color.purple)
         DynamicGraphView(data: [(xInt1, yInt1), (xInt2, yInt2), (xInt3, yInt3)], colors: [Color.purple, Color.orange])
 
-        // TODO: Can't pan in a ScrollView
         ScrollView {
             LazyVStack {
                 DynamicGraphView(x: x, y: y3, showZones: true)
